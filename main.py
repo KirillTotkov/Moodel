6c697c6c3d2aa2433ca7f9b574fe687550075be0
KirillTotkov <kirillatot@gmail.com>
2024-04-22 19:16:43 +0300

    Добавить запуск бота и загрузку обработчиков в бота

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello")
+from moodle import Moodle
+from loguru import logger
+from vkbottle import Bot
+from vkbottle.bot import Message
+from vkbot.handlers import labelers
+from config import labeler, bot
+
+
+for labeler in labelers:
+    bot.labeler.load(labeler)
+
+bot.run_forever()
