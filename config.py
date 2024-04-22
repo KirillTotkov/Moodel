0277c1d19b26ffb6704cab69acd45b5c44a6f887
KirillTotkov <kirillatot@gmail.com>
2024-04-22 17:34:36 +0300

    Добавить config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from vkbottle import API, BuiltinStateDispenser
+from vkbottle.bot import BotLabeler
+from moodle import Moodle
+
+
+load_dotenv()
+
+MOODLE_URL = os.getenv('MOODLE_URL')
+BOT_TOKEN = os.getenv('VK_BOT_TOKEN')
+MOODLE_TOKEN = os.getenv('MOODLE_TOKEN')
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+api = API(BOT_TOKEN)
+labeler = BotLabeler()
+state_dispenser = BuiltinStateDispenser()
+
+moodle = Moodle(MOODLE_URL, MOODLE_TOKEN)
--- database/sessions.py
+++ database/sessions.py
@@ -1,8 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from contextlib import contextmanager
-
-DATABASE_URL = "sqlite:///./example.db"
+from config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 
