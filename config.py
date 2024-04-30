09379086031f7bf732341f2a91fc23f6aa9700d3
KirillTotkov <kirillatot@gmail.com>
2024-04-30 12:11:53 +0300

    Обновить файл config.py

--- config.py
+++ config.py
@@ -1,18 +1,14 @@
 import os
 from dotenv import load_dotenv
-from vkbottle import API, Bot, BuiltinStateDispenser
+from vkbottle import API, Bot, BuiltinStateDispenser, DocMessagesUploader
 from vkbottle.bot import BotLabeler
 from moodle import Moodle
-
 
 load_dotenv()
 
 MOODLE_URL = os.getenv('MOODLE_HOST')
 BOT_TOKEN = os.getenv('VK_BOT_TOKEN')
-MOODLE_TOKEN = os.getenv('MOODLE_TOKEN')
 DATABASE_URL = os.getenv('DATABASE_URL')
-
-print(MOODLE_URL)
 
 api = API(BOT_TOKEN)
 labeler = BotLabeler()
@@ -23,5 +19,7 @@ bot = Bot(
     labeler=labeler,
     state_dispenser=state_dispenser,
 )
+
+uploader = DocMessagesUploader(bot.api)
 
 moodle = Moodle(MOODLE_URL + '/webservice/rest/server.php', "")
